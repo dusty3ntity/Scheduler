af670bdcc4e89f9292a48f5a9439272e2998a6a9
kotiakhova <kotiakhovaa@gmail.com>
2020-10-20 20:11:33 +0300

    Created a "new event" endpoint (#10)

--- backend/api/routes/events.ts
+++ backend/api/routes/events.ts
@@ -1,15 +1,29 @@
-import { Response, Router } from "express";
+import { Response, Router, Request } from "express";
 
 import Event from "../../models/event";
 
 const initEventsRoutes = (router: Router): void => {
-	const route = Router();
+  const route = Router();
 
-	router.use("/events", route);
-	route.get("/", async (_, res: Response) => {
-		const events = await Event.find({});
-		return res.json(events).status(200);
-	});
+  router.use("/events", route);
+
+  route.get("/", async (_, res: Response) => {
+    const events = await Event.find({});
+    return res.json(events).status(200);
+  });
+
+  route.post("/", async (req: Request, res: Response) => {
+    try {
+      const event = new Event(req.body);
+      const success = await event.save();
+      if (success) {
+        return res.status(201).end();
+      }
+      throw new Error("Problem saving changes");
+    } catch (err) {
+      return res.status(500).end();
+    }
+  });
 };
 
 export default initEventsRoutes;
