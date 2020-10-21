ab1cdb5c3b22732acfa1dc1920fff7533d458d6e
Andrii-Makarets <Andriy.makarets@gmail.com>
2020-10-21 21:10:29 +0300

    Seeded the db with some test events (#8)

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 
+import seedData from "./loaders/seeder";
 import initializeExpress from "./loaders/express";
 import initializeMongoose from "./loaders/mongoose";
 
@@ -8,6 +9,7 @@ const start = async () => {
 
 	initializeExpress({ app: app });
 	await initializeMongoose();
+	await seedData();
 	app.listen(5080, () => {
 		console.log("Server running on port 5080");
 	});
---
+++ backend/loaders/seeder.ts
@@ -0,0 +1,42 @@
+// @ts-ignore
+import moment from "moment";
+
+import Event from "../models/event";
+
+const seedData = async () =>{
+	const events = await Event.exists({});
+	if(events) {
+		return;
+	}
+	const success= await Event.insertMany(testEvents);
+	if(success){
+		console.log("Seeded test data");
+	} else {
+		throw new Error("Problem seeding data");
+	}
+};
+
+const testEvents = [
+	{
+		title: "Go to the dentist",
+		startDate: moment().add(1, "d").hour(7).minute(30),
+		endDate: moment().add(1, "d").hour(8).minute(30),
+	},
+	{
+		title: "Go to the sauna",
+		startDate: moment().add(1, "d").hour(8).minute(30),
+		endDate: moment().add(1, "d").hour(19).minute(30),
+	},
+	{
+		title: "Go to the barbershop",
+		startDate: moment().add(2, "d").hour(13).minute(0),
+		endDate: moment().add(2, "d").hour(14).minute(0),
+	},
+	{
+		title: "Live coding",
+		startDate: moment().add(3, "d").hour(17).minute(0),
+		endDate: moment().add(3, "d").hour(18).minute(45),
+	},
+];
+
+export default seedData;
