d393622a12727fd2d49de782e3d287563de2c624
Andrii Chaliuk <0632135592an@gmail.com>
2020-10-21 15:59:33 +0300

    Created a 'delete event' endpoint (#14)

--- backend/api/routes/events.ts
+++ backend/api/routes/events.ts
@@ -3,39 +3,51 @@ import { Response, Router, Request } from "express";
 import Event from "../../models/event";
 
 const initEventsRoutes = (router: Router): void => {
-  const route = Router();
+	const route = Router();
 
-  router.use("/events", route);
+	router.use("/events", route);
 
-  route.get("/", async (_, res: Response) => {
-    const events = await Event.find({});
-    return res.json(events).status(200);
-  });
+	route.get("/", async (_, res: Response) => {
+		const events = await Event.find({});
+		return res.json(events).status(200);
+	});
 
-  route.get("/:id", async (req: Request, res: Response) => {
-    try {
-      const event = (await Event.find({ _id: req.params.id }))[0];
-      if (event) {
-        return res.json(event).status(200);
-      }
-      return res.status(404).end();
-    } catch {
-      return res.status(500).end();
-    }
-  });
+	route.get("/:id", async (req: Request, res: Response) => {
+		try {
+			const event = (await Event.find({ _id: req.params.id }))[0];
+			if (event) {
+				return res.json(event).status(200);
+			}
+			return res.status(404).end();
+		} catch {
+			return res.status(500).end();
+		}
+	});
 
-  route.post("/", async (req: Request, res: Response) => {
-    try {
-      const event = new Event(req.body);
-      const success = await event.save();
-      if (success) {
-        return res.status(201).end();
-      }
-      throw new Error("Problem saving changes");
-    } catch (err) {
-      return res.status(500).end();
-    }
-  });
+	route.post("/", async (req: Request, res: Response) => {
+		try {
+			const event = new Event(req.body);
+			const success = await event.save();
+			if (success) {
+				return res.status(201).end();
+			}
+			throw new Error("Problem saving changes");
+		} catch (err) {
+			return res.status(500).end();
+		}
+	});
+
+	route.delete("/:id", async (req: Request, res: Response) => {
+		try {
+			const success = await Event.deleteOne({_id: req.params.id});
+			if (success.deletedCount) {
+				return res.status(200).end();
+			}
+			return res.status(404).end();
+		} catch (err) {
+			return res.status(500).end();
+		}
+	});
 };
 
 export default initEventsRoutes;
--- backend/loaders/mongoose.ts
+++ backend/loaders/mongoose.ts
@@ -2,11 +2,17 @@ import mongoose from "mongoose";
 import { Db } from "mongodb";
 
 const initializeMongoose = async (): Promise<Db> => {
-	const conection = await mongoose.connect("mongodb://localhost:27017/scheduler", {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
-	return conection.connection.db;
+	try {
+		const conection = await mongoose.connect("mongodb://localhost:27017/scheduler", {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		return conection.connection.db;
+	} catch (e) {
+		console.log("Server Error", e.message);
+		process.exit(1);
+	}
 };
 
 export default initializeMongoose;
