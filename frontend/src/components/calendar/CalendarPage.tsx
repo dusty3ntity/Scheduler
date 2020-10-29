8d9cd8fe9532d7b303aabdbcc2ac657dcca01c82
Svitlana Kyrychenko <svitlanakyrychenko1008@gmail.com>
2020-10-29 15:00:18 +0200

    implemented sidebar (#18)

--- frontend/src/components/calendar/CalendarPage.tsx
+++ frontend/src/components/calendar/CalendarPage.tsx
@@ -2,21 +2,11 @@ import React from "react";
 import TopPanel from "./TopPanel";
 
 const CalendarPage: React.FC = () => {
-<<<<<<< HEAD
-	return <div id="calendar-page" className="page"></div>;
-||||||| 9e93e11
-	return (
-		<div id="calendar-page" className="page">
-			Calendar
-		</div>
-	);
-=======
 	return (
 		<div id="calendar-page" className="page">
 			<TopPanel />
 		</div>
 	);
->>>>>>> 5e86070d4f9f1fd24cb3480325a3de5abd003e65
 };
 
 export default CalendarPage;
