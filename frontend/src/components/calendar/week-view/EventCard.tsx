5cea899dee753efe1b303dd43cf90bb9cc9cb071
Andrii-Makarets <Andriy.makarets@gmail.com>
2020-11-19 13:11:38 +0200

    Make the events open when clicked on the calendar

--- frontend/src/components/calendar/week-view/EventCard.tsx
+++ frontend/src/components/calendar/week-view/EventCard.tsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import moment from "moment";
 
-import { Event } from "../../../models/events";
-import { getDayEventStyles } from "../../../utils/calendar";
+import {Event} from "../../../models/events";
+import {getDayEventStyles} from "../../../utils/calendar";
 
 export interface EventCardProps {
 	event: Event;
 }
 
-const EventCard: React.FC<EventCardProps> = ({ event }) => {
+const EventCard: React.FC<EventCardProps> = ({event}) => {
 	const timeInterval = `${moment(event.startDate).format("HH:mm")} – ${moment(event.endDate).format("HH:mm")}`;
+	const eventCardRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const handleClick = (e: MouseEvent): void => {
+			const target = e.target as HTMLDivElement;
+			if (eventCardRef.current && eventCardRef.current.contains(target)) {
+				e.stopImmediatePropagation();
+				console.log(event);
+			}
+		};
+		document.addEventListener("mousedown", handleClick);
+		return (): void => {
+			document.removeEventListener("mousedown", handleClick);
+		};
+	});
 
 	return (
-		<div className="event-card" style={getDayEventStyles(event)} key={event.startDate.toString()}>
+		<div className="event-card" style={getDayEventStyles(event)} key={event.startDate.toString()} ref={eventCardRef}>
 			<span className="title">{event.title}</span>
 			<span className="time-interval">{timeInterval}</span>
 		</div>
--- frontend/src/components/calendar/week-view/WeekDay.tsx
+++ frontend/src/components/calendar/week-view/WeekDay.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 
-import { Day, Event } from "../../../models/events";
+import {Day, Event} from "../../../models/events";
 import EventCard from "./EventCard";
 
 export interface WeekDayProps {
 	day: Day;
 }
 
-const WeekDay: React.FC<WeekDayProps> = ({ day }) => {
+const WeekDay: React.FC<WeekDayProps> = ({day}) => {
+	const dayRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const handleClick = (e: MouseEvent): void => {
+			const target = e.target as HTMLDivElement;
+			if (dayRef.current && dayRef.current.contains(target)) {
+				console.log(e.clientY - target.getBoundingClientRect().top);
+			}
+		};
+		document.addEventListener("mousedown", handleClick);
+		return (): void => {
+			document.removeEventListener("mousedown", handleClick);
+		};
+	});
+
 	return (
-		<div className="week-day">
+		<div className="week-day" ref={dayRef}>
 			{day.events.map((event: Event) => (
-				<EventCard key={event.title} event={event} />
+				<EventCard key={event.title} event={event}/>
 			))}
 		</div>
 	);
