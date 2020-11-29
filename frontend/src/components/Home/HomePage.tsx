99f48ac731312a3264d043c32f6314fd9e6ae3b0
Svitlana Kyrychenko <svitlanakyrychenko1008@gmail.com>
2020-11-29 16:30:30 +0200

    add design

--- frontend/src/components/Home/HomePage.tsx
+++ frontend/src/components/Home/HomePage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { combineClassNames } from "../../utils/components/classNames";
 
 import "./home-page.scss";
 
@@ -10,9 +11,13 @@ export const HomePage: React.FC = () => {
 
 	return (
 		<div id="home-page" className="page">
-			<h1>Home</h1>
+			<img className={combineClassNames("pic", "wave1")} src="/images/wave1.png" />
+			<img className={combineClassNames("pic", "wave2")} src="/images/wave2.png" />
+			<img className={combineClassNames("pic", "person-skate")} src="/images/personSkate.png" />
+			<img className={combineClassNames("pic", "person-skate2")} src="/images/personSkate2.png" />
+			<h1 className="head">Home</h1>
 
-			<Link to="/calendar" className="btn">
+			<Link to="/calendar" className={combineClassNames ("btn", "home")}>
 				Go to calendar
 			</Link>
 		</div>
