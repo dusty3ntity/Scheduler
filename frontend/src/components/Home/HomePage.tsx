ead766eb5d7daffff12cffaa795c2dea546b95c2
Vadym Ohyr <dusty3ntity@gmail.com>
2020-11-30 10:24:24 +0200

    Added alts on images

--- frontend/src/components/Home/HomePage.tsx
+++ frontend/src/components/Home/HomePage.tsx
@@ -15,10 +15,10 @@ export const HomePage: React.FC = () => {
 		<div id="home-page" className="page">
 			<h1 className="title">{t("home_page_title")}</h1>
 
-			<img className="pic wave1" src="/images/wave1.png" />
-			<img className="pic wave2" src="/images/wave2.png" />
-			<img className="pic person-skate" src="/images/personSkate.png" />
-			<img className="pic person-skate2" src="/images/personSkate2.png" />
+			<img className="pic wave1" src="/images/wave1.png" alt="" />
+			<img className="pic wave2" src="/images/wave2.png" alt="" />
+			<img className="pic person-skate" src="/images/personSkate.png" alt="" />
+			<img className="pic person-skate2" src="/images/personSkate2.png" alt="" />
 
 			<Link to="/calendar" className="btn">
 				{t("home_page_calendar_button")}
