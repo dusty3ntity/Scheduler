70085a4b222b31e2b39d14f47f02401e7bfd192d
Svitlana Kyrychenko <svitlanakyrychenko1008@gmail.com>
2020-11-30 11:23:09 +0200

    added some button hover

--- frontend/src/components/Common/Icons/MenuIcon.tsx
+++ frontend/src/components/Common/Icons/MenuIcon.tsx
@@ -9,7 +9,7 @@ import "./icons.scss";
  * Icon name: menu-24px
  */
 export const MenuIcon: React.FC<ComponentProps> = ({ id, className, ...props }) => (
-	<svg id={id} className={combineClassNames("icon menu-icon", className)} viewBox="0 0 24 24" {...props}>
+	<svg id={id} className={combineClassNames("icon menu-icon", className, "dark-background", "menu-icon")} viewBox="0 0 24 24" {...props}>
 		<path d="M0 0h24v24H0V0z" fill="none" />
 		<path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z" />
 	</svg>
--- frontend/src/components/Common/Icons/PlusIcon.tsx
+++ frontend/src/components/Common/Icons/PlusIcon.tsx
@@ -9,7 +9,7 @@ import "./icons.scss";
  * Icon name: add-24px
  */
 export const PlusIcon: React.FC<ComponentProps> = ({ id, className, ...props }) => (
-	<svg id={id} className={combineClassNames("icon plus-icon", className, "light-background")} viewBox="0 0 24 24" {...props}>
+	<svg id={id} className={combineClassNames("icon plus-icon", className)} viewBox="0 0 24 24" {...props}>
 		<path d="M0 0h24v24H0V0z" fill="none" />
 		<path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z" />
 	</svg>
